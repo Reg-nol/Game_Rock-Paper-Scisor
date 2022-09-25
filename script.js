72e9b6a118887311a96b4f37fd397c4e3a49674c
Reginald <bountyhunter.sequeira@gmail.com>
2022-09-25 23:54:17 +0530

    Added tiktaktoe game html and css , not ompleted just for saving purpose

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice (){
+    let a = Math.floor(Math.random()*3);
+    if(a===0){
+        return "Rock";
+    } else if(a===1){
+        return "Paper";
+    } else {
+        return "Scissor";
+    }
+}
+
+console.log(getComputerChoice());
