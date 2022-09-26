64d32d847ef3757642118dbfae2db0ce79b33272
Reginald <bountyhunter.sequeira@gmail.com>
2022-09-26 20:14:12 +0530

    Completed rock paper scisor game

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 function getComputerChoice (){
     let a = Math.floor(Math.random()*3);
     if(a===0){
-        return "Rock";
+        return "rock";
     } else if(a===1){
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissor";
+        return "scisor";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection , computerSelection) {
+    this.playerSelection = playerSelection;
+    this.computerSelection = computerSelection;
+    if(this.playerSelection === this.computerSelection){
+        return "Its a Draw , You selected "+this.playerSelection+" and Computer selected "+this.computerSelection;
+    }else{
+           if(this.playerSelection == "rock" && this.computerSelection == "scisor"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Player wins!";
+           } else if (this.playerSelection == "scisor" && this.computerSelection == "rock"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Computer wins!";
+           }else if(this.playerSelection == "rock" && this.computerSelection == "paper"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Computer wins!";
+           }else if (this.playerSelection == "paper" && this.computerSelection == "rock"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Player wins!";
+           }else if (this.playerSelection == "scisor" && this.computerSelection == "paper"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Player wins!";
+           }else if (this.playerSelection == "paper" && this.computerSelection == "scisor"){
+            return "playr selection was "+this.playerSelection+" and computer selected "+ this.computerSelection+ " = Computer  wins!";
+           }
+           
+    }
+   
+}
+
+
+            
+            
+            const computerSelectionVar = getComputerChoice();
+        
+        function game()
+        {
+        
+        
+            
+            let a = prompt("Input Your Tool" , "Rock , Paper or Scisor");
+            let playerSelectionVar = a.toLowerCase();
+          let result = playRound(playerSelectionVar, computerSelectionVar);
+          return result;
+        
+    }
+        for(i = 1 ; i < 6 ; i++){
+        let resultEnd = game();
+        console.log(resultEnd);
+        }
